22cd9ddc7ceef32e58547dc06bc08a3269364346
fayazur-rahman <fayazur8@gmail.com>
2024-11-14 18:18:56 +0600

    first commit

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Tickets"),
+      ),
+      body: Center(
+        child: Text("Ticket Info"),
+      ),
+      bottomNavigationBar: Container(child: Text("Bottom"),),
+    );
+  }
+}
